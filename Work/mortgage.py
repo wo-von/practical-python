fd0fb0b666278842c2fff965e6fc432fa15eec79
sina hosseinizad <ssh@ammonit.com>
2020-06-20 11:46:10 +0200

    add the solution for mortgage with extra pament months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+totalPaid = 0.0
+EXTRA_PAYMENT_START_MONTH = 49
+EXTRA_PAYMENT_END_MONTH = 109
+EXTRA_PAYMENT = 1000.0
+monthCounter = 0
+
+# while EXTRA_PAYMENT_DURATION > 0:
+#     principal = principal * ( 1 + rate/12) - payment - EXTRA_PAYMENT
+#     totalPaid += (payment + EXTRA_PAYMENT)
+#     EXTRA_PAYMENT_DURATION -= 1
+#     monthCounter += 1
+
+while principal > 0:
+    
+    if monthCounter >= EXTRA_PAYMENT_START_MONTH and monthCounter <= EXTRA_PAYMENT_END_MONTH:
+         principal = principal * ( 1 + rate/12) - payment - EXTRA_PAYMENT
+         totalPaid += payment + EXTRA_PAYMENT
+    else:
+        principal = principal * ( 1 + rate/12) - payment
+        totalPaid += payment
+    monthCounter += 1
+
+totalPaid = round(totalPaid, 2)
+print("total amount paid is", totalPaid, "and it was paid over",monthCounter,"months")
