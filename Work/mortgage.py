ead6b4b937234553c24c346018c4e8c665ce90d9
sina hosseinizad <ssh@ammonit.com>
2020-06-20 12:03:02 +0200

    fix the problem in mortgage.py with wxtra payment in the last month

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,8 +6,8 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 totalPaid = 0.0
-EXTRA_PAYMENT_START_MONTH = 49
-EXTRA_PAYMENT_END_MONTH = 109
+EXTRA_PAYMENT_START_MONTH = 1
+EXTRA_PAYMENT_END_MONTH = 12
 EXTRA_PAYMENT = 1000.0
 monthCounter = 0
 
@@ -20,11 +20,19 @@ monthCounter = 0
 while principal > 0:
     
     if monthCounter >= EXTRA_PAYMENT_START_MONTH and monthCounter <= EXTRA_PAYMENT_END_MONTH:
-         principal = principal * ( 1 + rate/12) - payment - EXTRA_PAYMENT
-         totalPaid += payment + EXTRA_PAYMENT
+        if principal <= payment + EXTRA_PAYMENT:
+            totalPaid += principal + EXTRA_PAYMENT
+            principal -= principal + EXTRA_PAYMENT
+        else:
+            principal = principal * ( 1 + rate/12) - payment - EXTRA_PAYMENT
+            totalPaid += payment + EXTRA_PAYMENT
     else:
-        principal = principal * ( 1 + rate/12) - payment
-        totalPaid += payment
+        if principal <= payment:
+            totalPaid += principal
+            principal -= principal
+        else:
+            principal = principal * ( 1 + rate/12) - payment
+            totalPaid += payment
     monthCounter += 1
     print(monthCounter, round(totalPaid,2), round(principal,2))
 
