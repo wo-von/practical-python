c2cf517337dfc848fee0672613fc84bb50fdcbd3
sina hosseinizad <ssh@ammonit.com>
2020-06-22 22:59:05 +0200

    ch01 ex30 turn the pcost.py into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,21 @@
 #
 # Exercise 1.27
 
-with open('Work/Data/portfolio.csv', 'rt') as f:
-    header = next(f)
-    stockData = []
-    for line in f:
-        line = line.strip()
-        stockData.append(line.split(','))
+def portfolio_cost(filename):
+    '''gets a csv file of the shares, their qunatity and value
+    returns the total cost for buying them all
+    has a header which should be nexed
+    '''
+    with open('Work/Data/portfolio.csv', 'rt') as f:
+        header = next(f)
+        stockData = []
+        for line in f:
+            line = line.strip()
+            stockData.append(line.split(','))
+    totalCost = 0
+    for item in stockData:
+        totalCost += int(item[1]) * float(item[2])
+    return totalCost
 
-
-totalCost = 0
-for item in stockData:
-    totalCost += int(item[1]) * float(item[2])
-print("Total cost %.2f" % totalCost)
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
