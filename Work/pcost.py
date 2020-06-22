fade42bab24c9fa2e53814c59c900ae8b29dd67c
sina hosseinizad <ssh@ammonit.com>
2020-06-22 22:21:07 +0200

    ch01 ex27 add cod for pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    header = next(f)
+    stockData = []
+    for line in f:
+        line = line.strip()
+        stockData.append(line.split(','))
+
+
+totalCost = 0
+for item in stockData:
+    totalCost += int(item[1]) * float(item[2])
+print("Total cost %.2f" % totalCost)
