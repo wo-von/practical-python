74c5a49010d285eccdefdd1551d2a2dd7f5c73d8
sina hosseinizad <ssh@ammonit.com>
2020-06-23 09:16:23 +0200

    ch01 ex32 change the pcost.py such that it uses csv library

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,21 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
     '''gets a csv file of the shares, their qunatity and value
     returns the total cost for buying them all
-    has a header which should be nexed
+    has a header which should be nexted
     '''
-    with open(f'Work/{filename}', 'rt') as f:
+    with open(f'Work/{filename}') as f:
+        rows = csv.reader(f)
         header = next(f)
         stockData = []
-        for line in f:
-            line = line.strip()
-            stockData.append(line.split(','))
-            
+        for line in rows:
+            # csv reader does much of the stuff itself, no need to manually do them
+            # line = line.strip()
+            stockData.append(line)
     totalCost = 0
     for item in stockData:
         try:
