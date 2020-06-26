a960b79191589a08709ea48b4c8d0685c32d175c
sina hosseinizad <ssh@ammonit.com>
2020-06-26 09:58:13 +0200

    ex2.4 add the draft from p1 for the report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,36 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def portfolio_cost(filename):
+    '''gets a csv file of the shares, their qunatity and value
+    returns the total cost for buying them all
+    has a header which should be nexted
+    '''
+    # it is assumed that we are in the Work directory
+    with open(filename) as f:
+        rows = csv.reader(f)
+        header = next(f)
+        stockData = []
+        for line in rows:
+            # csv reader does much of the stuff itself, no need to manually do them
+            # line = line.strip()
+            stockData.append(line)
+    totalCost = 0
+    for item in stockData:
+        try:
+            totalCost += int(item[1]) * float(item[2])
+        except ValueError:
+            print("bad row", item)
+    return totalCost
+
+# argv are the argumentsa passed through the terminal, a list of strings, depending on how many have been passed
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
