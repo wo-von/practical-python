a55d3bda3bbedae1946e79ef8595de71187f48c7
sina hosseinizad <ssh@ammonit.com>
2020-07-29 11:19:21 +0200

    Ex3.1 refactor the code and write it functionwise, remove gain/loss calculations, since the result was not being used for now

--- Work/report.py
+++ Work/report.py
@@ -12,21 +12,21 @@ def read_portfolio(filename: str = 'Data/portfolio.csv') -> list:
     opens a given portfolio file and reads it into a list of dictionaries
     '''
     select = ['name', 'shares', 'price']
-    operator = [str, int, float]
+    type = [str, int, float]
     with open(filename) as f:
         portfolio = []
         rows = csv.reader(f)
         headers = next(rows)
         indices = [headers.index(s) for s in select]
         
-        portfolio = [{col:func(row[index]) for col, func, index in zip(select, operator, indices)} for row in rows]
+        portfolio = [{col:fun(row[ind]) for col, fun, ind in zip(select, type, indices)} for row in rows]
     return portfolio
 
-def read_prices(filename: str = 'Data/prices.csv') -> dict:
+def read_prices(pricesfilename: str = 'Data/prices.csv') -> dict:
     '''
     reads a set of prices such as this into a dictionary where the keys of the dictionary are the stock names and the values in the dictionary are the stock prices.
     '''
-    with open(filename) as f:
+    with open(pricesfilename) as f:
         rows = csv.reader(f)
         priceDict = {}
         try:
@@ -47,40 +47,23 @@ def make_report(stockList: list, priceDic: dict) -> list:
             report.append((item['name'], item['shares'], priceDic[item['name']], round(priceDic[item['name']] - item['price'], 2)))
     return report
 
-def setPortfolioFile():
+def print_report(report: list):
     '''
-    If filename has been passed through teminal, pass it, if not, use default
+    prints out the report
     '''
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-    else:
-        filename = 'Data/portfolio.csv'
+    headers = ('Name', 'Shares', 'Price', 'Change')
 
-# Not sure if this is the correct way to start a program
-setPortfolioFile()
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+    for row in report:
+        print('%10s %10d %10.2f %10.2f' % row)
 
-gainLoss = 0
-currentValue = 0
-whatIHad = 0
-portfolio = read_portfolio()
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+portfolio = read_portfolio(filename)
 prices = read_prices()
-for myShares in portfolio:
-    whatIHad += myShares['shares'] * myShares['price']
-
-for myShares in portfolio:
-    # only get the prices, if we have them
-    if myShares['name'] in prices:
-        currentValue += myShares['shares'] * prices[myShares['name']]
-gainLoss = round((whatIHad- currentValue), 2)
-# print("Current value of the portfolio", currentValue, "with gain/loss of", gainLoss)
-# in 2.11 we assume we have this tuple
-headers = ('Name', 'Shares', 'Price', 'Change')
-
-print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-
-print(f'{10*"-"} {10*"-"} {10*"-"} {10*"-"}')
-
 report = make_report(portfolio, prices)
-for name, shares, price, change in report:
-    price = "$" + str(price)
-    print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
+print_report(report)
