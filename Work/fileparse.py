8adebb8b47b9d7c9e4d4087c235d262d51b5e3b5
sina hosseinizad <ssh@ammonit.com>
2020-07-29 13:39:54 +0200

    Ex3.4 select the columns needed, if epecified in fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     '''
     reads a csv file into a 
     '''
@@ -11,10 +11,18 @@ def parse_csv(filename):
         rows = csv.reader(f)
         # read the file headers
         headers = next(rows)
+        
+        if select:
+            indices = [headers.index(s) for s in select]
+            headers = select
+        else:
+            indices = []
         records = []
         for row in rows:
             if not row: # skip rows with no data
                 continue
+            if indices:
+                row = [row[index] for index in indicesz]
             record = dict(zip(headers, row))
             records.append(record)
     return records
