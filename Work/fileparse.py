e722ad91003983837ff76850244816a891223913
sina hosseinizad <ssh@ammonit.com>
2020-07-29 13:57:58 +0200

    Ex3.6 also work with csv files with no header (it might be possible to be done easier, like this sentence)

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,28 +3,32 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select = None, types = None):
+def parse_csv(filename, select = None, types = None, has_headers = True):
     '''
     reads a csv file into a 
     '''
     with open(filename) as f:
         rows = csv.reader(f)
         # read the file headers
-        headers = next(rows)
-        
-        if select:
-            indices = [headers.index(s) for s in select]
-            headers = select
-        else:
-            indices = []
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(s) for s in select]
+                headers = select
+            else:
+                indices = []
         records = []
         for row in rows:
             if not row: # skip rows with no data
                 continue
-            if indices:
-                row = [row[index] for index in indicesz]
+            if has_headers:
+                if indices:
+                    row = [row[index] for index in indicesz]
             if types:
                 row = [func(value) for func, value in zip(types, row)]
-            record = dict(zip(headers, row))
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
     return records
