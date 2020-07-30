23341dd4e3913f8cc2864f79ba08350ca4823a74
sina hosseinizad <ssh@ammonit.com>
2020-07-30 16:11:35 +0200

    Ex3.17 change the fileparse, so that it is more flexible and works with any type of iterator, the caller now needs to open the file etc.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,39 +5,40 @@ import csv
 
 def parse_csv(filename, select = None, types = None, has_headers = True, delimiter = ',', silence_errors = False):
     '''
-    reads a csv file into a dictionary (in case there are headers) or into a tuple with raw files
+    reads a csv iterable into a dictionary (in case there are headers) or into a tuple with raw files. The iterable should be opened and passed by the caller
     '''
     if select != None and has_headers == False:
         raise RuntimeError("select argument requires column headers")
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter = delimiter)
-        # read the file headers
-        if has_headers:
-            headers = next(rows)
-        else:
-            headers = []
-        if select:
-            indices = [headers.index(s) for s in select]
-            headers = select
-        else:
-            indices = []
-        records = []
-           
-        for n, row in enumerate(rows):
-            try:    
-                if not row: # skip rows with no data
-                    continue
-                if select:
-                    row = [row[index] for index in indices]
-                if types:
-                    row = [func(value) for func, value in zip(types, row)]
-                if has_headers:
-                    record = dict(zip(headers, row))
-                else:
-                    record = tuple(row)
-                records.append(record)
-            except ValueError as e:
-                    if not silence_errors:
-                        print(f"Row {n + 1}: Couldn't convert {row}")
-                        print(f"Row {n + 1}: Reason {e}")     
+    if type(filename) == str:
+        raise RuntimeError("Caller should send the iterator and not the iterable itself")
+    rows = csv.reader(filename, delimiter = delimiter)
+    # read the file headers
+    if has_headers:
+        headers = next(rows)
+    else:
+        headers = []
+    if select:
+        indices = [headers.index(s) for s in select]
+        headers = select
+    else:
+        indices = []
+    records = []
+        
+    for n, row in enumerate(rows, start=1):
+        try:    
+            if not row: # skip rows with no data
+                continue
+            if select:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(value) for func, value in zip(types, row)]
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+        except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {n}: Couldn't convert {row}")
+                    print(f"Row {n}: Reason {e}")     
     return records
