57c561490d710f7410af06d47a8c3a8404e8d827
sina hosseinizad <ssh@ammonit.com>
2020-07-30 14:44:09 +0200

    ex3.14 fix pcost which was falsely done in previous commit usinf fileparse instead of report

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,20 +5,15 @@
 import csv
 import sys
 
-import fileparse
+import report
 
 def portfolio_cost(filename):
     '''gets a csv file of the shares, their qunatity and value
     returns the total cost for buying them all
     has a header which should be nexted
-    '''
-    
-    
-    portfolio = fileparse.parse_csv(filename, select = ['shares', 'price'], types = [int, float])
-    totalCost = 0.0
-    for port in portfolio:
-        totalCost += port['shares'] * port['price']
-    return totalCost
+    '''  
+    portfolio = report.read_portfolio(filename)
+    return sum(s['shares'] * s['price'] for s in portfolio)
 
 # argv are the arguments passed through the terminal, a list of strings, depending on how many have been passed
 if len(sys.argv) == 2:
