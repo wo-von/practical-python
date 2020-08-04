d0151f96dcdffaf7dc2014d3fbdabbccc34627e9
sina hosseinizad <ssh@ammonit.com>
2020-08-04 10:43:19 +0200

    Ex6.3 make the container object portfolio more 'pythonic' by adding built-in methods that are used in other pyhton containers

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -9,6 +9,12 @@ class Portfolio(object):
         self._holdings = holdings
     def __iter__(self):
         return self._holdings.__iter__()
+    def __len__(self):
+        return len(self._holdings)
+    def __getitem__(self, index):
+        return self._holdings[index]
+    def __contains__(self, name):
+        return any([s.name == name for s in self._holdings])
     @property
     def total_cost(self):
         return sum([s.cost for s in self._holdings])
