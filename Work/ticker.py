730ee0136f630f9c5ca45ae37866b97f8cac82ad
sina hosseinizad <ssh@ammonit.com>
2020-08-05 15:42:35 +0200

    Ex6.15 refactor the code, using more generator expressions (simplify?)

--- Work/ticker.py
+++ Work/ticker.py
@@ -11,37 +11,22 @@ from follow import follow
 import report
 import tableformat
 
-def select_columns(rows, indices):
-    for row in rows:
-        yield[row[index] for index in indices]
-
-def convert_types(rows, types):
-    for row in rows:
-        yield [func(val) for func, val in zip(types, row)]
-
-def make_dicts(rows, headers):
-    for row in rows:
-        yield dict(zip(headers, row))
-
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
-
-def parse_stock_data(lines):
+def parse_stock_data(lines, portobj, select):
     '''
     gets the output of a generator and returns csv reader object
     '''
     rows = csv.reader(lines)
-    rows = select_columns(rows, [0, 1, 4])
-    rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, ['name', 'price', 'change'])
+    rows = ([row[index] for index in [0, 1, 4]] for row in rows)
+    rows = ([func(val) for func, val in zip([str, float, float], row)] for row in rows)
+    rows = ({headers: item for headers, item in zip(select, row)} for row in rows)
+    rows = (row for row in rows if row['name'] in portobj)
     return rows
 
 def ticker(portfile, logfile, fmt):
+    headers = ['name', 'price', 'change']
     portfolio = report.read_portfolio(portfile)
-    rows = filter_symbols(parse_stock_data(follow(logfile)), portfolio)
+    rows = parse_stock_data(follow(logfile), portfolio, headers)
     formatter = tableformat.create_formatter(fmt)
-    formatter.headings(['name', 'price', 'change'])
+    formatter.headings(headers)
     for row in rows:
         formatter.row([ str(e) for e in row.values()])
