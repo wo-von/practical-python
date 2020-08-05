bf73df17a3db08c699df8354433b32531b035b1f
sina hosseinizad <ssh@ammonit.com>
2020-08-05 15:00:08 +0200

    Ex6.14 replace list comprehensions with generator expressions in portfolio for better memory performance

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -14,12 +14,12 @@ class Portfolio(object):
     def __getitem__(self, index):
         return self._holdings[index]
     def __contains__(self, name):
-        return any([s.name == name for s in self._holdings])
+        return any(s.name == name for s in self._holdings)
     def __repr__(self):
         return f'Portfolio({self._holdings})'
     @property
     def total_cost(self):
-        return sum([s.cost for s in self._holdings])
+        return sum(s.cost for s in self._holdings)
     def tabulate_shares(self):
         from collections import Counter
         total_shares = Counter()
