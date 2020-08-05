0453f342e3a4e2c2ab516528ccf0dcfc4c259035
sina hosseinizad <ssh@ammonit.com>
2020-08-05 09:23:34 +0200

    Ex6.6 produce the data to be consumed later by a generator

--- Work/follow.py
+++ Work/follow.py
@@ -7,17 +7,23 @@
 import os
 import time
 
-f = open('Data/stocklog.csv')
-f.seek(0, os.SEEK_END)
+def follow(filename):
+    f = open(filename)
+    f.seek(0, os.SEEK_END)
+    while True:
+        line = f.readline()
+        if line == '':
+            continue
+        yield line
+    f.close()
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)
-        continue
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+if __name__ == '__main__':
+    portfolio = report.read_portfolio('Data/portfolio.csv')
+    for line in follow('Data/stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+                print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
