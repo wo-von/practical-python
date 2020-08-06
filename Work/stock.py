d7870a38ccf12acc73be77cfcf4edcf364a89a79
sina hosseinizad <ssh@ammonit.com>
2020-08-06 14:05:10 +0200

    Ex7.9 use lambda functions to siimplify the code

--- Work/stock.py
+++ Work/stock.py
@@ -2,9 +2,9 @@
 from typedproperty import typedproperty, String, Integer, Float
 
 class Stock(object):
-    name = typedproperty('name', str)
-    shares = typedproperty('shares', int)
-    price = typedproperty('price', float)
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
